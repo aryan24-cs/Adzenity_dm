b626f9df2f895f355eb680f61f2a6b3733358c90
Sunil Baghel <sunilbaghel93100@gmail.com>
2026-02-08 20:40:37 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -28,7 +28,12 @@ const connectDB = async () => {
     }
 
     try {
-        const db = await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/adzenity');
+        const uri = process.env.MONGODB_URI;
+        if (!uri) {
+            throw new Error('MONGODB_URI environment variable is not defined.');
+        }
+
+        const db = await mongoose.connect(uri);
         isConnected = db.connections[0].readyState;
         console.log('MongoDB Connected successfully');
     } catch (error) {
